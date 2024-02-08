e8fa7e59c72067c6937eea8635af165c90bea68b
csura-briloerp <csura@briloerp.com>
2024-02-08 15:40:43 -0600

    Correcciones ortográficas Ejercicio1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,8 +1,10 @@
-﻿/*aciendo uso de la entrada de datos, indíquele al usuario que ingrese 2 números,
+﻿/*
+Haciendo uso de la entrada de datos, indíquele al usuario que ingrese 2 números,
 almacénelos en sus respectivas variables y determine lo siguiente:
 ➢ Cuál de ellos es el número mayor (No puede hacer uso de estructuras).
 ➢ Obtenga la raíz cubica de un número entero ingresado por teclado (puede
-ser uno de los números anteriormente ingresado o solicite uno nuevo).*/
+ser uno de los números anteriormente ingresado o solicite uno nuevo).
+*/
 
 
 using System;
@@ -15,18 +17,18 @@ namespace Ejercicio1
         {
             double num1, num2, resultado, resRaiz;
             int numRaiz;
-            Console.WriteLine("Ingrese  el primer numero ");
+            Console.WriteLine("Ingrese el primer número ");
             num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Ingrese  el segundo numero ");
+            Console.WriteLine("Ingrese el segundo número ");
             num2 = Convert.ToDouble(Console.ReadLine());
             resultado = Math.Max(num1, num2);
-            Console.WriteLine("El numero mayor es " + resultado);
+            Console.WriteLine("El número mayor es " + resultado);
 
             Console.WriteLine("--------------------------------");
-            Console.WriteLine("Ingrese el numero Entero a obtener raiz cubica ");
+            Console.WriteLine("Ingrese el número Entero a obtener raíz cúbica ");
             numRaiz = int.Parse(Console.ReadLine());
             resRaiz = Math.Pow(numRaiz, 1.0 / 3.0);
-            Console.WriteLine("La raiz cubica de " + numRaiz + " es " + resRaiz);
+            Console.WriteLine("La raíz cúbica de " + numRaiz + " es " + resRaiz);
         }
     }
 }
