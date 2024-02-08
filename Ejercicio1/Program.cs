cfe7fb2f6d56a8b561b4dfe8b01c98d720724e98
IsaiAlvarezF <135726163+IsaiAlvarezF@users.noreply.github.com>
2024-02-08 15:33:24 -0600

    Ejercicio2s

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*aciendo uso de la entrada de datos, indíquele al usuario que ingrese 2 números,
+almacénelos en sus respectivas variables y determine lo siguiente:
+➢ Cuál de ellos es el número mayor (No puede hacer uso de estructuras).
+➢ Obtenga la raíz cubica de un número entero ingresado por teclado (puede
+ser uno de los números anteriormente ingresado o solicite uno nuevo).*/
+
+
+using System;
 
 namespace Ejercicio1
 {
@@ -6,7 +13,20 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double num1, num2, resultado, resRaiz;
+            int numRaiz;
+            Console.WriteLine("Ingrese  el primer numero ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese  el segundo numero ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            resultado = Math.Max(num1, num2);
+            Console.WriteLine("El numero mayor es " + resultado);
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Ingrese el numero Entero a obtener raiz cubica ");
+            numRaiz = int.Parse(Console.ReadLine());
+            resRaiz = Math.Pow(numRaiz, 1.0 / 3.0);
+            Console.WriteLine("La raiz cubica de " + numRaiz + " es " + resRaiz);
         }
     }
 }
