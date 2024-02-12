8cbe938f40e5d9a252c2cec63d19d9c4ce078669
VaneGueva <2511732023@mail.utec.edu.sv>
2024-02-11 23:43:52 -0600

    Update Program.cs
    SE agrego ejercicio 3

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese un numero a evaluar(Solo se permiten numeros):");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"El numero ingresado es {(num > 0 ? "POSITIVO" : (num < 0 ? "NEGATIVO" : "CERO"))}");
