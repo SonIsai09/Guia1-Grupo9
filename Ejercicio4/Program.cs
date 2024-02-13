4008420a7efd9f9104c0d219c5d352e8dbad7039
CiscoParzi <frankguevara634@gmail.com>
2024-02-12 20:53:30 -0600

    Ejercicio 4

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AñosBisiestos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el año de inicio del rango:");
+            int inicio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el año de fin del rango:");
+            int fin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Años bisiestos dentro del rango:");
+
+            for (int year = inicio; year <= fin; year++)
+            {
+                if (EsBisiesto(year))
+                {
+                    Console.WriteLine(year);
+                }
+            }
+        }
+
+        static bool EsBisiesto(int year)
+        {
+            return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        }
+    }
+}
