a05cd39191e51aa16db377efc1de0c01b14fe071
IsaiAlvarezF <135726163+IsaiAlvarezF@users.noreply.github.com>
2024-02-13 08:05:11 -0600

    Cambio  de concatenación

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Ingrese un número entero a evaluar (Solo se permiten números enteros):");
 int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"El número ingresado es {(num > 0 ? "POSITIVO" : (num < 0 ? "NEGATIVO" : "CERO"))}");
+Console.WriteLine("El número ingresado es " + (num > 0 ? "POSITIVO" : (num < 0 ? "NEGATIVO" : "CERO")));
